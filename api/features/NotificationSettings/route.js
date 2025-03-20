b3f25f5638e786b358e72e26e6c4473335751803
omkumarvaghani <omvaghani161@gmail.com>
2025-03-20 11:45:35 +0530

    push request

--- api/features/NotificationSettings/route.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const NotificationSetting = require("./model");
-const { verifyLoginToken } = require("../../authentication");
-
-
-
-module.exports = router;
